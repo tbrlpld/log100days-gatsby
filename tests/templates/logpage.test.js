9d1f18c8095666fde64b505fe7affcdafd7aa0c4
Tibor Leupold <tibor@lpld.io>
2020-08-18 18:13:18 -1000

    Prepare testing with jest

---
+++ tests/templates/logpage.test.js
@@ -0,0 +1,9 @@
+/* eslint-env jest */
+
+const { fixRelativeLinksForGatsby } = require('../../src/templates/logpage.js')
+
+describe('Fix relative links for Gatsby', () => {
+  it('exists', () => {
+    fixRelativeLinksForGatsby()
+  })
+})
