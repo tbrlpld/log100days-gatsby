0f1707ddcf45a0badec8dca0876703c8749a2828
Tibor Leupold <tibor@lpld.io>
2020-08-19 12:11:47 -1000

    Fix slug when link ends with `.md/`

--- tests/templates/logpage.test.js
+++ tests/templates/logpage.test.js
@@ -87,4 +87,28 @@ describe('Update local markdown to slug definition', () => {
     expect(fixedHtml).toContain('href="sibling/"')
     expect(fixedHtml).not.toContain('href="sibling.md/"')
   })
+
+  it('leaves external link as is (even if ending in .md).', () => {
+    const html = '<div><a href="http://example.com/site.md"></a></div>'
+    const fixedHtml = fixRelativeLinksForGatsby(html)
+    expect(fixedHtml).toContain('href="http://example.com/site.md"')
+  })
+
+  it('leaves external link (https) as is (even if ending in .md).', () => {
+    const html = '<div><a href="https://example.com/site.md"></a></div>'
+    const fixedHtml = fixRelativeLinksForGatsby(html)
+    expect(fixedHtml).toContain('href="https://example.com/site.md"')
+  })
+
+  it('leaves mail link as is (even if ending in .md).', () => {
+    const html = '<div><a href="mailto:somebody@example.md"></a></div>'
+    const fixedHtml = fixRelativeLinksForGatsby(html)
+    expect(fixedHtml).toContain('href="mailto:somebody@example.md"')
+  })
+
+  it('leaves tel link as is.', () => {
+    const html = '<div><a href="tel:+123456789"></a></div>'
+    const fixedHtml = fixRelativeLinksForGatsby(html)
+    expect(fixedHtml).toContain('href="tel:+123456789"')
+  })
 })
